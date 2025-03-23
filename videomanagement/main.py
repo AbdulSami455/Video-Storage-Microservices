596fa6a7d8791bd4fc997bc4f056983529a04bf3
Muhammad Abdul Sami <msami.bese22seecs@student.nust.edu.pk>
2025-03-23 10:50:27 +0500

    Update main.py

--- videomanagement/main.py
+++ videomanagement/main.py
@@ -25,7 +25,7 @@ s3_client = boto3.client(
     aws_secret_access_key=AWS_SECRET_KEY,
 )
 
-client = MongoClient("mongodb+srv://msamibese22seecs:i9UvBXQv6HsdOqvY@cluster0.ozyev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient("")
 log_db = client["LogDatabase"]
 log_collection = log_db["logs"]
 
